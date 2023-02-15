ea45f40d631faf79349800a0b9b6b4b3ed8a12bd
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-15 15:20:16 -0500

    Div to button tag

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -2,15 +2,15 @@ import { MouseEventHandler } from "react";
 
 //Button base
 interface Button {
-	children: React.ReactNode;
-	className?: string;
-	onClick?: MouseEventHandler;
-	width?: number;
+  children: React.ReactNode;
+  className?: string;
+  onClick?: MouseEventHandler;
+  width?: number;
 }
 export const Button = ({ width, className, children, onClick }: Button) => {
-	return (
-		<div width={width} className={className} onClick={onClick}>
-			{children}
-		</div>
-	);
+  return (
+    <button width={width} className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
