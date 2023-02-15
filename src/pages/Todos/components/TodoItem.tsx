a1063dcb40b7f2b01fd476347d2224cefce2417a
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-15 15:20:26 -0500

    Add layout

--- src/pages/Todos/components/TodoItem.tsx
+++ src/pages/Todos/components/TodoItem.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { Button } from "@/components/Buttons";
 import { Checkmark, Close } from "@/components/Icons";
 import styled from "styled-components";
+import { useState } from "react";
 
 interface Props {
   completeTodo: (params: number) => void;
@@ -12,30 +13,65 @@ interface Props {
 const StyledTodoItem = styled.div`
   display: flex;
   border: 1px solid black;
+  padding: 0.5rem;
+  gap: 0.5rem;
+  & > .controls {
+    display: flex;
+    align-items: center;
+  }
+  & > .body {
+    flex: 1;
+    & > .header {
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      & > .title {
+        flex: 1;
+      }
+    }
+  }
 `;
 
 function TodoItem({ completeTodo, deleteTodo, todo }: Props) {
+  const [details, setShowDetails] = useState(false);
   const todoClass = classNames({
     complete: todo.complete,
     deleted: todo.timeDeleted,
   });
+
+  const showDetails = () => {
+    details ? setShowDetails(false) : setShowDetails(true);
+  };
+
   return (
-    <StyledTodoItem className={todoClass} id={`todo-${todo.id}`}>
+    <StyledTodoItem
+      className={todoClass}
+      id={`todo-${todo.id}`}
+      onClick={showDetails}
+    >
       <div className="controls">
         <Button onClick={() => completeTodo(todo.id)}>
           <Checkmark animate />
         </Button>
-        <Button onClick={() => deleteTodo(todo.id)}>
-          <Close animate />
-        </Button>
       </div>
-      <div className="details">
+      <div className="body">
         <div className="header">
-          <h3 className="title">{todo.title}</h3>
-          <div className="difficulty">{todo.difficulty}</div>
+          <span className="title">{todo.title}</span>
+          {todo.date && <div className="due-date">Due: {todo.date}</div>}
+          <Button onClick={() => deleteTodo(todo.id)}>
+            <Close animate />
+          </Button>
         </div>
-        <div className="description">{todo.description}</div>
-        <div className="due-date">{todo.date}</div>
+        {details && (
+          <>
+            {todo.description && (
+              <div className="description">{todo.description}</div>
+            )}
+            {todo.difficulty !== 0 && (
+              <div className="difficulty">Effort: {todo.difficulty}</div>
+            )}
+          </>
+        )}
       </div>
     </StyledTodoItem>
   );
