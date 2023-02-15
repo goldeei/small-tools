4d2793c871f2568c2001e62ca42c19a0576a3201
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-15 15:41:58 -0500

    Add edit and chevron down icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,16 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faXmark,
+  faPenToSquare,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 
-interface Icon {
-	animate?: boolean;
+interface Props {
+  animate?: boolean;
 }
 
-export const Checkmark = ({ animate }: Icon) => {
-	return (
-		<FontAwesomeIcon icon={faCheck} className={animate ? "animated" : ""} />
-	);
+export const Checkmark = ({ animate }: Props) => {
+  return (
+    <FontAwesomeIcon icon={faCheck} className={animate ? "animated" : ""} />
+  );
 };
 
-export const Close = ({ animate }: Icon) => {
-	return <FontAwesomeIcon icon={faXmark} className={animate ? "animated" : ""} />;
+export const Close = ({ animate }: Props) => {
+  return (
+    <FontAwesomeIcon icon={faXmark} className={animate ? "animated" : ""} />
+  );
+};
+export const Edit = ({ animate }: Props) => {
+  return (
+    <FontAwesomeIcon
+      icon={faPenToSquare}
+      className={animate ? "animated" : ""}
+    />
+  );
+};
+export const DownCaret = ({ animate }: Props) => {
+  return (
+    <FontAwesomeIcon
+      icon={faChevronDown}
+      className={animate ? "animated" : ""}
+    />
+  );
 };
