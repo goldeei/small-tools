6a8e81ad0c00f2905f7941de11055c6120577910
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-14 19:55:25 -0500

    Update TodoList to Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
 import Navbar from "@/components/Navbar";
-import { Wrapper } from "@/components/Layout"
+import { Wrapper } from "@/components/Layout";
 
 import Home from "@/pages/Home";
-import TodoList from "@/pages/TodoList";
+import TodoList from "@/pages/Todos";
 
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<Wrapper>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/todo-list" element={<TodoList />} />
-				</Routes>
-			</Wrapper>
-		</>
-	);
+  return (
+    <>
+      <Wrapper>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todo-list" element={<TodoList />} />
+        </Routes>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
