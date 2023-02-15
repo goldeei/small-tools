b648e69e1a72c0460bb138a5181cec2b1c623337
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-15 14:44:33 -0500

    Refactor addTodo to be contained in AddTodo

--- src/pages/Todos/components/AddTodo.tsx
+++ src/pages/Todos/components/AddTodo.tsx
@@ -1,76 +1,92 @@
 import React, { useEffect, useState } from "react";
 
 const initial = {
-	id: 0,
-	title: "",
-	description: "",
-	difficulty: 0,
-	date: "",
-	complete: false,
-	timeDeleted: false,
+  id: 0,
+  title: "",
+  description: "",
+  difficulty: 0,
+  date: "",
+  complete: false,
+  timeDeleted: false,
 };
 
 interface AddTodo {
-	addTodo: (params: TodoItem) => void;
+  todos: TodoItem[];
+  updateTodos: (params: TodoItem[]) => void;
 }
 
-export default function addTodo({ addTodo }: AddTodo) {
-	const [data, setData] = useState(initial);
+export default function addTodo({ todos, updateTodos }: AddTodo) {
+  const [data, setData] = useState(initial);
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setData({
-			...data,
-			[name]: value,
-		});
-	};
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (!data) return;
-		addTodo(data);
-		setData(initial);
-		const form = document.getElementById("new-todo-form");
-		form?.querySelectorAll("input").forEach((input) => (input.value = ""));
-	};
-	return (
-		<div>
-			Add Todo
-			<form id="new-todo-form" onSubmit={handleSubmit}>
-				<input
-					onChange={handleInputChange}
-					type="text"
-					id="new-todo-title"
-					name="title"
-					placeholder="Title"
-					required
-				/>
-				<input
-					onChange={handleInputChange}
-					type="text"
-					id="new-todo-description"
-					name="description"
-					placeholder="Description"
-				/>
-				<input
-					onChange={handleInputChange}
-					type="range"
-					id="new-todo-difficulty"
-					name="difficulty"
-					max="5"
-					step="1"
-					placeholder="Difficulty"
-				/>
-				<input
-					onChange={handleInputChange}
-					type="date"
-					id="new-todo-due"
-					name="date"
-					placeholder="Todo Due"
-				/>
-				<button id="add-todo" type="submit">
-					Add Todo
-				</button>
-			</form>
-		</div>
-	);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!data) return;
+    addTodo(data);
+    setData(initial);
+    const form = document.getElementById("new-todo-form");
+    form?.querySelectorAll("input").forEach((input) => (input.value = ""));
+  };
+  const addTodo = (todo: TodoItem) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: todos.length,
+        title: todo.title,
+        description: todo.description,
+        difficulty: todo.difficulty,
+        date: todo.date,
+        complete: false,
+        timeDeleted: null,
+      },
+    ];
+    return updateTodos(newTodos);
+  };
+  return (
+    <div>
+      Add Todo
+      <form id="new-todo-form" onSubmit={handleSubmit}>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          id="new-todo-title"
+          name="title"
+          placeholder="Title"
+          required
+        />
+        <input
+          onChange={handleInputChange}
+          type="text"
+          id="new-todo-description"
+          name="description"
+          placeholder="Description"
+        />
+        <input
+          onChange={handleInputChange}
+          type="range"
+          id="new-todo-difficulty"
+          name="difficulty"
+          max="5"
+          step="1"
+          placeholder="Difficulty"
+        />
+        <input
+          onChange={handleInputChange}
+          type="date"
+          id="new-todo-due"
+          name="date"
+          placeholder="Todo Due"
+        />
+        <button id="add-todo" type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Todos/index.tsx
+++ src/pages/Todos/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import TodoItem from "./components/TodoItem";
 import AddTodo from "./components/AddTodo";
 import useLocalStorage from "@/hooks/useLocalStorage";
@@ -33,6 +33,9 @@ export default function Todos() {
   const [storedTodos, setStoredTodos] = useLocalStorage("Todos", "");
   const [todos, setTodos] = useState(storedTodos);
 
+  const updateTodos = useCallback((updatedTodos: TodoItem[]) => {
+    setTodos(updatedTodos);
+  }, []);
   const deleteTodo = (id: number) => {
     const newTodos = [...todos];
     document.getElementById(`todo-${id}`)?.classList.add("deleted");
@@ -54,21 +57,7 @@ export default function Todos() {
       return setTodos(newTodos);
     }, 1000);
   };
-  const addTodo = (todo: TodoItem) => {
-    const newTodos = [
-      ...todos,
-      {
-        id: todos.length,
-        title: todo.title,
-        description: todo.description,
-        difficulty: todo.difficulty,
-        date: todo.date,
-        complete: false,
-        timeDeleted: null,
-      },
-    ];
-    return setTodos(newTodos);
-  };
+
   const clearTodos = () => {
     const newTodos = todos.map((todo: TodoItem) => {
       document.getElementById(`todo-${todo.id}`)?.classList.add("deleted");
@@ -87,7 +76,7 @@ export default function Todos() {
   }, [todos]);
   return (
     <TodoContainer>
-      {/* <AddTodo addTodo={addTodo} /> */}
+      <AddTodo todos={todos} updateTodos={updateTodos} />
       {/* <button onClick={clearTodos}>Clear</button> */}
       <div className="deleted">
         <h1>Deleted</h1>
@@ -101,58 +90,6 @@ export default function Todos() {
         <h1>Active</h1>
         <ListTodos todos={todos} status={"active"} />
       </div>
-      {/* <div className="todos-todolist">
-        <h4>Todos</h4>
-        
-        {todos.length > 0 &&
-          todos.filter((todo: TodoItem) => todo.complete === false) &&
-          todos.map((todo: TodoItem) => {
-            if (!todo.complete === true && todo.timeDeleted === null) {
-              return (
-                <TodoItem
-                  key={todo.id}
-                  completeTodo={completeTodo}
-                  deleteTodo={deleteTodo}
-                  todo={todo}
-                />
-              );
-            }
-          })}
-      </div>
-      <div className="todos-todolist">
-        <h4>Completed</h4>
-        {todos.length > 0 &&
-          todos.filter((todo: TodoItem) => todo.complete === true) &&
-          todos.map((todo: TodoItem) => {
-            if (todo.complete === true && todo.timeDeleted === null) {
-              return (
-                <TodoItem
-                  key={todo.id}
-                  completeTodo={completeTodo}
-                  deleteTodo={deleteTodo}
-                  todo={todo}
-                />
-              );
-            }
-          })}
-      </div>
-      <div>
-        <h4>Recently Deleted</h4>
-        {todos.length > 0 &&
-          todos.filter((todo: TodoItem) => todo.timeDeleted !== null) &&
-          todos.map((todo: TodoItem) => {
-            if (todo.timeDeleted !== null) {
-              return (
-                <TodoItem
-                  key={todo.id}
-                  completeTodo={completeTodo}
-                  deleteTodo={deleteTodo}
-                  todo={todo}
-                />
-              );
-            }
-          })}
-      </div> */}
     </TodoContainer>
   );
 }
