6c8e63bbe7b7b1b7690ba42e39e13d66e0656d07
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-15 17:47:19 -0500

    Add controls toggle, style for legibility

--- src/pages/Todos/components/TodoItem.tsx
+++ src/pages/Todos/components/TodoItem.tsx
@@ -29,11 +29,13 @@ const StyledTodoItem = styled.div`
     align-items: center;
   }
   & > .controls {
-    height: fit-content;
+    height: 100%;
     padding: 0 0.5rem;
     gap: 0.25rem;
     display: flex;
     justify-content: space-around;
+    background-color: blue;
+    border-left: 1px solid black;
     &.expanded {
       flex-direction: column;
       padding: 0.5rem 0;
@@ -41,6 +43,11 @@ const StyledTodoItem = styled.div`
     border-radius: 0.25rem;
     background-color: #c0efff;
     box-shadow: 0px 0px 2px 0px black;
+  }
+  & > .more {
+    border: 1px solid black;
+    background-color: blue;
+    padding: 0 0.5rem;
   }
   & > .body {
     flex: 1;
@@ -59,7 +66,8 @@ const StyledTodoItem = styled.div`
 `;
 
 function TodoItem({ completeTodo, deleteTodo, todo }: Props) {
-  const [details, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
+  const [showControls, setShowControls] = useState(false);
   const todoClass = classNames({
     complete: todo.complete,
     deleted: todo.timeDeleted,
@@ -67,17 +75,42 @@ function TodoItem({ completeTodo, deleteTodo, todo }: Props) {
 
   const hasDetails = todo.description || todo.difficulty ? true : false;
 
-  const showDetails = () => {
+  const toggleDetails = () => {
+    if (showDetails) {
+      setShowControls(false);
+    }
     if (hasDetails) {
-      return details ? setShowDetails(false) : setShowDetails(true);
+      return setShowDetails(!showDetails);
     }
   };
-
+  const toggleControls = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    return setShowControls(!showControls);
+  };
+  const Controls = ({ showDetails, showControls }) => {
+    if (showDetails || showControls) {
+      return (
+        <div className={`controls ${showDetails ? "expanded" : ""}`}>
+          <Button onClick={(e) => e.stopPropagation()}>
+            <Close animate />
+          </Button>
+          {hasDetails && (
+            <Button onClick={(e) => e.stopPropagation()}>
+              <DownCaret />
+            </Button>
+          )}
+          <Button onClick={(e) => e.stopPropagation()}>
+            <Edit />
+          </Button>
+        </div>
+      );
+    }
+  };
   return (
     <StyledTodoItem
       className={todoClass}
       id={`todo-${todo.id}`}
-      onClick={showDetails}
+      onClick={toggleDetails}
     >
       <div className="complete">
         <Button onClick={(e) => e.stopPropagation()}>
@@ -85,11 +118,11 @@ function TodoItem({ completeTodo, deleteTodo, todo }: Props) {
         </Button>
       </div>
       <div className="body">
-        <div className={`header ${details ? "expanded" : ""}`}>
+        <div className={`header ${showDetails ? "expanded" : ""}`}>
           <span className="title">{todo.title}</span>
           {todo.date && <div className="due-date">Due: {todo.date}</div>}
         </div>
-        {details && (
+        {showDetails && (
           <>
             {todo.description && (
               <div className="description">{todo.description}</div>
@@ -100,22 +133,12 @@ function TodoItem({ completeTodo, deleteTodo, todo }: Props) {
           </>
         )}
       </div>
-      <div className={`controls ${details ? "expanded" : ""}`}>
-        <Button>
+      {!showDetails && (
+        <Button className="more" onClick={(e) => toggleControls(e)}>
           <Ellipses orientation="vertical" />
         </Button>
-        <Button onClick={(e) => e.stopPropagation()}>
-          <Close animate />
-        </Button>
-        {hasDetails && (
-          <Button onClick={(e) => e.stopPropagation()}>
-            <DownCaret />
-          </Button>
-        )}
-        <Button onClick={(e) => e.stopPropagation()}>
-          <Edit />
-        </Button>
-      </div>
+      )}
+      <Controls showDetails={showDetails} showControls={showControls} />
     </StyledTodoItem>
   );
 }
