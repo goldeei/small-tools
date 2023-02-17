958e6285396c9df3b50f89b38ae62415fea1511c
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-17 16:06:11 -0500

    Remove refactored functions

--- src/pages/Todos/index.tsx
+++ src/pages/Todos/index.tsx
@@ -24,65 +24,20 @@ const TodoContainer = styled(ToolContainer)`
 `;
 
 export default function Todos() {
-  const [storedTodos, setStoredTodos] = useLocalStorage("Todos", "");
-  const [todos, setTodos] = useState(storedTodos);
-
-  const updateTodos = useCallback((updatedTodos: TodoItem[]) => {
-    setTodos(updatedTodos);
-  }, []);
-  const deleteTodo = (id: number) => {
-    const newTodos = [...todos];
-    document.getElementById(`todo-${id}`)?.classList.add("deleted");
-    newTodos[id].timeDeleted = Date.now();
-    setTimeout(() => {
-      return setTodos(newTodos);
-    }, 1000);
-  };
-  const completeTodo = (id: number) => {
-    const newTodos = [...todos];
-    if (newTodos[id].complete) {
-      newTodos[id].complete = false;
-      document.getElementById(`todo-${id}`)?.classList.remove("complete");
-    } else {
-      newTodos[id].complete = true;
-      document.getElementById(`todo-${id}`)?.classList.add("complete");
-    }
-    setTimeout(() => {
-      return setTodos(newTodos);
-    }, 1000);
-  };
-
-  const clearTodos = () => {
-    const newTodos = todos.map((todo: TodoItem) => {
-      document.getElementById(`todo-${todo.id}`)?.classList.add("deleted");
-      return {
-        ...todo,
-        timeDeleted: Date.now(),
-      };
-    });
-    setTimeout(() => {
-      return setTodos(newTodos);
-    }, 1000);
-  };
-  useEffect(() => {
-    setStoredTodos(todos);
-    console.table(todos);
-  }, [todos]);
   return (
     <TodoContainer>
-      <AddTodo todos={todos} updateTodos={updateTodos} />
       {/* <button onClick={clearTodos}>Clear</button> */}
       <div className="deleted">
         <h1>Deleted</h1>
-        <ListTodos todos={todos} status={"deleted"} />
+        <ListTodos status={"deleted"} />
       </div>
       <div className="completed">
         <h1>Completed</h1>
-        <ListTodos todos={todos} status={"completed"} />
+        <ListTodos status={"completed"} />
       </div>
       <div className="active">
         <h1>Active</h1>
-        <ListTodos todos={todos} status={"active"} />
+        <ListTodos status={"active"} />
       </div>
     </TodoContainer>
   );
