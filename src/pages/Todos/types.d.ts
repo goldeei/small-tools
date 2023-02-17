ca253516fd94cbfbeb5c8f2909167d9ae1dc00c6
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-17 16:06:21 -0500

    Add UpdateTodo interface

--- src/pages/Todos/types.d.ts
+++ src/pages/Todos/types.d.ts
@@ -1,9 +1,13 @@
 interface TodoItem {
-	complete: boolean;
-	date?: string;
-	description?: string;
-	timeDeleted: date;
-	difficulty?: number;
-	id: number;
-	title: string;
+  complete: boolean;
+  date?: string;
+  description?: string;
+  timeDeleted: date;
+  difficulty?: number;
+  id: number;
+  title: string;
+}
+
+interface UpdateTodo {
+  (e: React.MouseEvent, id: number, key: string, value: any): void;
 }
