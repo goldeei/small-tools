7c5ee91b411e6cdaf2eadc3d4bf5593d21ee8db7
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-20 15:50:04 -0500

    Setup base for theming

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,24 @@ import Home from "@/pages/Home";
 import TodoList from "@/pages/Todos";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setDark] = useState(false);
+
+  const setTheme = (current) => {
+    console.log(current);
+    document.documentElement?.setAttribute(
+      "data-theme",
+      isDark ? "dark" : "light"
+    );
+    setDark(current);
+  };
+
   return (
     <RecoilRoot>
-      <div className={blocks.pageWrapper}>
+      <button onClick={() => setTheme(!isDark)}>Test</button>
+      <div className={`${blocks.pageWrapper}`}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
