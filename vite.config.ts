38472c9ac1e3735fa82c7b32e1c1623e78948845
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-20 14:50:39 -0500

    install/config postcss, css modules

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
-	},
-	plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+  },
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
 });
