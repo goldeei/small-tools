3c12b0bbc6b190ed6291a732e5aa2cb03707a189
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-20 19:36:26 -0500

    Move html specific styling to App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,25 @@ import { RecoilRoot, useRecoilState } from "recoil";
 import Navbar from "@/components/Navbar";
 import ThemeControl from "./components/ThemeControl";
 
-import blocks from "@/assets/styles/layout/blocks.module.css";
+import styles from "./App.module.css";
 
 import Home from "@/pages/Home";
 import TodoList from "@/pages/Todos";
 
 function App() {
-  // const [isDark, setDark] = useRecoilState(userThemeAtom);
-  // const setTheme = (current) => {
-  //   console.log(current);
-  //   document.documentElement?.setAttribute(
-  //     "data-theme",
-  //     isDark ? "dark" : "light"
-  //   );
-  //   setDark(current);
-  // };
-
   return (
     <RecoilRoot>
-      <ThemeControl />
-      <div className={`${blocks.pageWrapper}`}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todo-list" element={<TodoList />} />
-        </Routes>
+      <div className={`${styles.wrapper}`}>
+        <div className={`${styles.container}`}>
+          <div>
+            <ThemeControl />
+            <Navbar />
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todo-list" element={<TodoList />} />
+          </Routes>
+        </div>
       </div>
     </RecoilRoot>
   );
