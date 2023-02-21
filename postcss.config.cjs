45ce3f8d79d8406ab721cad567a2c721c379e9af
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-20 19:13:36 -0500

    Add custom-media queries

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("autoprefixer"),
+    require("postcss-custom-media"),
     require("postcss-preset-env")({ stage: 1 }),
+    require("autoprefixer"),
     require("cssnano"),
   ],
 };
