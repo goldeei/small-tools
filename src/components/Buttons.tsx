4173ef3f792751b53a8f933dad727ca015cbe967
Jake Goldfarb <jake.goldiee@gmail.com>
2023-02-21 20:24:20 -0500

    Remove styled components

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,22 +1,15 @@
 import { MouseEventHandler } from "react";
-import styled from "styled-components";
 
-//Button base
 interface Button {
   children: React.ReactNode;
   className?: string;
   onClick?: MouseEventHandler;
 }
 
-const StyledButton = styled.button`
-  background-color: transparent;
-  border: none;
-`;
-
-export const Button = ({ width, className, children, onClick }: Button) => {
+export const Button = ({ className, children, onClick }: Button) => {
   return (
-    <StyledButton className={className} onClick={onClick}>
+    <button className={className} onClick={onClick}>
       {children}
-    </StyledButton>
+    </button>
   );
 };
